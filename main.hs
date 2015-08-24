5abe6d483948e1821fecb9ff520349017c2954e4
Undo-all <undoall2@gmail.com>
2015-08-24 10:30:43 -0400

    first commit

---
+++ main.hs
@@ -0,0 +1,122 @@
+import Control.Monad.State
+import Control.Monad.Writer
+import Control.Monad.Except
+import Data.Char
+
+data CompilerState = CompilerState
+                   { cellPtr :: Int
+                   , stackPtr :: Int
+                   , tempCells :: [Int]
+                   }
+
+type Compiler = StateT CompilerState (WriterT String (Except String)) ()
+
+runCompiler :: Compiler -> Either String String
+runCompiler = runExcept . execWriterT . flip evalStateT (CompilerState 0 0 [])
+
+moveTo :: Int -> Compiler
+moveTo to | to < 0 = throwError "attempt to move to negative memory location"
+moveTo to = 
+    do cp <- gets cellPtr
+       tell $ replicate (abs $ to - cp)
+                        (if to > cp then '>' else '<')
+       modify $ \x -> x { cellPtr = to }
+
+guideTo :: Int -> Compiler
+guideTo to = 
+    do tcs <- gets tempCells
+       if to `elem` tcs 
+         then guideTo (to - 1)
+         else moveTo to
+
+guideToOffset :: Int -> Compiler
+guideToOffset offset =
+    do sp <- gets stackPtr
+       guideTo (sp - (1 + offset))
+
+zero :: Int -> Compiler
+zero cell =
+    do moveTo cell
+       tell "[-]"
+
+assign :: Int -> Int -> Compiler
+assign cell val = 
+    do moveTo cell
+       tell $ replicate (abs val)
+                        (if val > 0 then '+' else '-')
+
+push :: Int -> Compiler 
+push val = 
+    do sp <- gets stackPtr
+       zero sp 
+       assign sp val
+       modify $ \x -> x { stackPtr = sp + 1 } 
+
+pushTemp :: Int -> Compiler
+pushTemp val = 
+    do push val
+       modify $ \x -> x { tempCells = (stackPtr x - 1) : tempCells x }
+
+pop' :: Compiler
+pop' = 
+    do sp <- gets stackPtr
+       when (sp <= 0) $ throwError "attempt to pop from empty stack"
+       modify $ \x -> x { stackPtr = sp - 1 }
+
+pop :: Compiler
+pop = 
+    do pop'
+       sp <- gets stackPtr
+       tcs <- gets tempCells
+       when ((sp - 1) `elem` tcs) pop
+
+data Value = Int Int
+           | Char Char deriving (Eq, Show)
+
+data Ins = Drop
+         | Out
+         | Add deriving (Eq, Show)
+
+data Expr = Literal Value
+          | Instruction Ins
+          | IfElse [Expr] [Expr]
+          | When [Expr]
+          | While [Expr] deriving (Eq, Show)
+
+compileValue :: Value -> Compiler
+compileValue (Int n) = push n
+compileValue (Char c) = push (ord c)
+
+compileIns :: Ins -> Compiler
+compileIns Drop = pop
+compileIns Out = 
+    do guideToOffset 0
+       tell "."
+       pop
+compileIns Add = 
+    do guideToOffset 0
+       tell "["
+       guideToOffset 1
+       tell "+"
+       guideToOffset 0
+       tell "-]"
+       pop
+
+compileIfElse = undefined
+compileWhen = undefined
+compileWhile = undefined
+
+compileExpr :: Expr -> Compiler
+compileExpr (Literal val)     = compileValue val
+compileExpr (Instruction ins) = compileIns ins
+compileExpr (IfElse xs ys)    = compileIfElse xs ys
+compileExpr (When xs)         = compileWhen xs
+compileExpr (While xs)        = compileWhile xs
+
+compileExprs :: [Expr] -> Either String String
+compileExprs xs = runCompiler $ last <$> mapM compileExpr xs 
+
+main = do
+    either putStrLn putStrLn $ compileExprs [Literal $ Int 50, Literal $ Int 50, Instruction Add, Instruction Out]
+    
+
